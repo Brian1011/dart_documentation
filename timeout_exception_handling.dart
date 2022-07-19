f0dc24a1e32d8ba0d5854be9350bd23aae0cb1b9
Brian Mutinda <mutindabrian26@gmail.com>
2022-07-19 12:40:15 +0300

    add comments

--- timeout_exception_handling.dart
+++ timeout_exception_handling.dart
@@ -1,7 +1,13 @@
+/*
+* This is a test of the timeout exception handling.
+* When the function takes more than 3 seconds to execute, an exception is thrown.
+* The exception is caught and the function returns a value.
+* */
 import 'dart:async';
 
 test() async {
   try {
+    // break function after 3 seconds and throw an exception
     await Future.delayed(Duration(seconds: 5)).timeout(Duration(seconds: 3));
     print('Test has taken 5 seconds');
   } on TimeoutException catch (e) {
